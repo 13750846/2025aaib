e506111e0e4da3eba992b9ba8904e4a49c2ce1d4
13750846 <13750846@me.mcu.edu.tw>
2025-02-19 11:57:00 +0800

    Add files via upload

---
+++ week01-1.cpp
@@ -0,0 +1,18 @@
+/// week01-1.cpp 瘋狂程設 兩數相加
+/// File-New empty 開新的檔案
+/// Ctrl-S 存檔成 week01-1.cpp 要記得加 .cpp
+/// 下週考試考這題,不用寫藍色的「」
+#include <stdio.h>的
+///檔名不能寫錯
+///stdio的意思是標準的standard input/output
+///有了 stdio.h 的外掛，才能用 scanf() printf()
+int main()
+{
+	int a, b; ///整數 a, b
+	scanf( "%d" , &a ); /// 正課剛剛教,掃描讀東西
+	scanf( "%d" , &b ); /// 可以用複製的,再改b
+
+	printf("%d", a+b ); /// 實習課有教,印東西
+
+}
+
