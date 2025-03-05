25b323782e3255bdd9568815e2e4869c5783dc3a
13750846 <13750846@me.mcu.edu.tw>
2025-03-05 12:11:43 +0800

    week03

---
+++ week03-3.cpp
@@ -0,0 +1,15 @@
+///week03-3.cpp
+#include <stdio.h>
+int main()
+{///從0開始，中間寫多少就跑多少
+    for(int i=0 ; i<5 ; i++){
+        printf("%d ",i );
+    }///電腦的for 迴圈 電腦從0開始
+
+    printf("\n"); ///跳行
+    ///問多少就跑多少次
+    for(int i=1; i<=5; i++) {
+        printf("%d " i);
+    }///人類的 for 迴圈 從1開始
+
+}
